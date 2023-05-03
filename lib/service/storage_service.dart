c18615076f56ad99b40266603e61ecaae29b73c8
kuntal <kuntaln05@gmail.com>
2023-05-03 12:53:53 +0530

    refactor

--- lib/service/storage_service.dart
+++ lib/service/storage_service.dart
@@ -7,25 +7,24 @@ import 'package:firebase_core/firebase_core.dart' as firebase_core;
 class StorageService{
   final firebase_storage.FirebaseStorage storage = firebase_storage.FirebaseStorage.instance;
 
+  /// upload file
   Future uploadFile(String filePath,String fileName)async{
     File file = File(filePath);
     try{
       await storage.ref('userProfilePic/$fileName').putFile(file);
     }
     on firebase_core.FirebaseException catch(e){
-      print('error:-------');
-      print(e.message);
+      throw e.message.toString();
     }
   }
 
+  /// get url from firebase
   Future<String> getImageUrl(String name)async{
     firebase_storage.ListResult results = await storage.ref('userProfilePic').listAll();
     String url='';
     for (var element in results.items) {
       if(element.name==name){
-         print('name: ${element.name}');
          url = await element.getDownloadURL();
-         print('upload url:----- $url');
          return url;
       }
     }
